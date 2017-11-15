08d9695d7912335d5b0acf533dad35617bd7bea7
Aaron Shaver <aaronshaverpdx@gmail.com>
2017-11-15 15:07:29 -0800

    bad arguments method and test

--- src/com/aaronshaver/Main.java
+++ src/com/aaronshaver/Main.java
@@ -4,8 +4,12 @@ public class Main {
 
 
     public static void main(String[] args) {
-        //console = new ConsoleOutput();
-
+        ConsoleOutput console = new ConsoleOutput();
+        if(args.length != 1)
+        {
+            console.writeBadArgumentsMessage();
+            System.exit(0);
+        }
 
         System.out.println(String.format("Input file is '%s'", args[0]));
     }
--- test/src/DeduplicationTests.java
+++ test/src/DeduplicationTests.java
@@ -5,8 +5,15 @@ import org.junit.Test;
 public class DeduplicationTests {
 
     @Test
-    public void testStartMessage() {
+    public void testWriteStartMessage() {
         ConsoleOutput console = new ConsoleOutput();
-        Assert.assertEquals("Deduplicating...", console.startMessage());
+        Assert.assertEquals("Deduplicating...", console.writeStartMessage());
+    }
+
+    @Test
+    public void testWriteBadArgumentsMessage() {
+        ConsoleOutput console = new ConsoleOutput();
+        Assert.assertEquals("Please use one argument, after the .jar file and a space, pointing to .JSON file",
+                console.writeBadArgumentsMessage());
     }
 }
